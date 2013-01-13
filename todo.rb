414c90d5b2ae2406efebd546f0371bc2052f04f4
Chris Caruso <carusochrisr@gmail.com>
2013-01-13 16:58:57 -0500

    added day switching

--- todo.rb
+++ todo.rb
@@ -4,18 +4,21 @@
 
 # Future features:
 # 1. Display only current day in list, with ability to view previous days as well. Future too?
-# 2. Add duration and comments section to database.
-# 3. Add additional coloring/status for tasks that are overdue or weren't done.
+# 2. Add additional coloring/status for tasks that are overdue or weren't done.
+# 3. Add duration and comments section to database.
 # 4. Clean up formatting.
 # 5. Add some sort of show/hide comments in home display.
 # 6. Ability to note amount of time spent on each task if desired...pomodoro count?
 # 7. Rating of quality of task performance?
 # 8. Add ability to shift position of items in list
+# 9. Can't add new notes in the past, but can edit?
 
 require 'sinatra'
 require 'data_mapper'
 require 'haml'
 require 'active_support/all'
+
+curday = Date.today
 
 #database setup
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/recall.db")
@@ -38,19 +41,24 @@ DataMapper.finalize.auto_upgrade!
 get '/' do
 	@notes = Note.all :order=>:id.desc
 	@title = 'All Notes'
-	haml :home
+	haml :home, :locals => {:curday => curday}
 end
 
 get '/nextday' do
-	puts "sleeptest!"
-	sleep 2
+	curday = curday + 1.day
+	redirect '/'
+end
+
+get '/prevday' do
+	curday = curday - 1.day
 	redirect '/'
 end
 
 post '/' do
 	n = Note.new
 	n.content = params[:content]
-	n.created_at = Date.today
+	#n.created_at = Date.today
+	n.created_at = curday
 	n.updated_at = Time.now
 	n.save
 	redirect '/'
@@ -59,7 +67,7 @@ end
 get '/:id' do
 	@note = Note.get params[:id]
 	@title = "Edit note ##{params[:id]}"
-	haml :edit
+	haml :edit, :locals => {:curday => curday}
 end
 
 put '/:id' do
