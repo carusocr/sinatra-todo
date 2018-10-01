37c90ddead380dcf3c8361a1f2bff09860141680
Chris Caruso <carusocr@ldc.upenn.edu>
2018-10-01 06:33:04 -0700

    rent

--- todo.rb
+++ todo.rb
@@ -72,8 +72,8 @@ DataMapper.finalize.auto_upgrade!
 
 def check_repeaters()
 	#pay rent first of month
-	if Date.today.day == 1
-		Note.first_or_create(:content=>"RENT",:created_at=>Date.today)
+	if Date.today.day == 25
+		Note.first_or_create(:content=>"MORTGAGE",:created_at=>Date.today)
 	end
 	Note.all(:repeater => true).each do |rep|
 		if rep.created_at.cwday == Date.today.cwday && rep.complete == true && rep.created_at != Date.today
